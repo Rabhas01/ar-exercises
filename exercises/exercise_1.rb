be5b0a70513279b72c09d78d163d9b3914eb1063
Rabhas <rabhas.vashisht@gmail.com>
2022-05-04 13:55:46 +0000

    Task-3 Delete store 3

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,8 +5,8 @@ puts "----------"
 
 # Your code goes below here ...
 
-store1 = Store.create(name: "Burnaby", annual_revenue:300000, men_apparel: true, women_apparel:true )
-store2 = Store.create(name: "Richmond", annual_revenue:1260000,  men_apparel:false, women_apparel:true )
-store3 = Store.create(name: "Gastown", annual_revenue:190000, men_apparel:true, women_apparel:false )
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
 
 puts Store.count(:all)
