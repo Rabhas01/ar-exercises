db692370775456d9528c65c9b422947c904a557c
Rabhas <rabhas.vashisht@gmail.com>
2022-05-04 21:20:11 +0000

    Task -6 add datat to employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "James", last_name: "Shower", hourly_rate: 20)
+@store1.employees.create(first_name: "Julia", last_name: "Brush", hourly_rate: 10)
+@store4.employees.create(first_name: "Dan", last_name: "Coffee", hourly_rate: 40)
+@store5.employees.create(first_name: "Zenna", last_name: "Dong", hourly_rate: 07)
+@store6.employees.create(first_name: "Missy", last_name: "Paste", hourly_rate: 90)
+@store1.employees.create(first_name: "Will", last_name: "Bor", hourly_rate: 40)
+@store2.employees.create(first_name: "Nat", last_name: "Figg", hourly_rate: 70)
