dc789578de4eb2205b52e10c8eaba06d25807278
Rabhas <rabhas.vashisht@gmail.com>
2022-05-04 14:35:19 +0000

    Task -5 find total, avegrage, and count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+company_revenue = Store.sum(:annual_revenue)
+puts "Company revenue is #{company_revenue}"
+
+avg_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue is #{avg_revenue}"
+
+million_plus_store = Store.where("annual_revenue > 1000000").count()
+puts "Stores with $1M or more in annual income #{million_plus_store}"
