6ba2a440952ece99715a9899d7e99a2acc010eec
Rabhas <rabhas.vashisht@gmail.com>
2022-05-04 11:42:00 +0000

    Task-1 create 3 stores in database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store 1 = Store.create(name: "Burnaby", annual_revenue:300000, carries men_apparel: true, carries women_apparel:true )
+store 1 = Store.create(name: "Richmond", annual_revenue:1260000, carries men_apparel:false , carries women_apparel:true )
+store 1 = Store.create(name: "Gastown", annual_revenue:190000 ,carries men_apparel:true, carries women_apparel:false )
